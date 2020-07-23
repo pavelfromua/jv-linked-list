1aab2320931c99fb8c102d37f29585cd532b1f6d
pavelfromua <50228485+pavelfromua@users.noreply.github.com>
2020-07-23 16:49:11 +0300

    result

---
+++ src/main/java/MyLinkedListInterface.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public interface MyLinkedListInterface<T> {
+
+    boolean add(T value);
+
+    void add(T value, int index);
+
+    boolean addAll(List<T> list);
+
+    T get(int index);
+
+    T set(T value, int index);
+
+    T remove(int index);
+
+    boolean remove(T t);
+
+    int size();
+
+    boolean isEmpty();
+}
